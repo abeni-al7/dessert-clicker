0b6163e09d158903759b8f087d63c32d81583bc5
abeni-al7 <abenezeralebachew3@gmail.com>
2025-04-10 21:15:22 +0300

    Move share logic back to the UI

--- app/src/main/java/com/example/dessertclicker/ui/DessertViewModel.kt
+++ app/src/main/java/com/example/dessertclicker/ui/DessertViewModel.kt
@@ -29,32 +29,6 @@ class DessertViewModel: ViewModel() {
     }
 
     /**
-     * Share desserts sold information using ACTION_SEND intent
-     */
-    fun shareSoldDessertsInformation(intentContext: Context, dessertsSold: Int, revenue: Int) {
-        val sendIntent = Intent().apply {
-            action = Intent.ACTION_SEND
-            putExtra(
-                Intent.EXTRA_TEXT,
-                intentContext.getString(R.string.share_text, dessertsSold, revenue)
-            )
-            type = "text/plain"
-        }
-
-        val shareIntent = Intent.createChooser(sendIntent, null)
-
-        try {
-            ContextCompat.startActivity(intentContext, shareIntent, null)
-        } catch (e: ActivityNotFoundException) {
-            Toast.makeText(
-                intentContext,
-                intentContext.getString(R.string.sharing_not_available),
-                Toast.LENGTH_LONG
-            ).show()
-        }
-    }
-
-    /**
     * Determine which dessert to show.
     */
     private fun determineDessertToShow(
